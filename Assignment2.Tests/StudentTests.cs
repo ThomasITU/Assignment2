abf01f61ba785e24408c10ab088c30a825ef84dd
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-09-22 15:54:48 +0200

    Add Tests and ImmutableStudent Record

--- Assignment2.Tests/StudentTests.cs
+++ Assignment2.Tests/StudentTests.cs
@@ -17,9 +17,70 @@ namespace Assignment2.Tests
             
             //Act
             
-            
             //Assert
             Assert.Equal("id: 12345, name: My Jensen, status: Active, start date: 30/08/2020, end date: 17/06/2023, graduation date: 30/06/2023", student.ToString());
+        }
+
+        [Fact]
+        public void ToString_given_an_instance_of_ImmutableStudent_prints_the_ImmutableStudent_information()
+        {
+            //arrange
+            DateTime startDate = new DateTime(2020, 8, 30);
+            DateTime endDate = new DateTime(2023, 6, 17);
+            DateTime graduationDate = new DateTime(2023, 6, 30);
+            var Istudent = new ImmutableStudent(123456, "Thomas", "Wolgast", startDate, endDate, graduationDate);
+            
+            //Act
+            
+            //Assert
+            Assert.Equal(
+                "id: 123456, name: Thomas Wolgast, status: Active, 30/08/2020, end date: 17/06/2023, graduation date: 30/06/2023",
+                Istudent.ToString());
+        }
+
+        [Fact]
+        public void given_two_ImmutabelStudents_began_the_same_day_evaluates_to_true()
+        {
+            //arrange
+            DateTime startDate = new DateTime(2020, 8, 30);
+            DateTime endDate = new DateTime(2023, 6, 17);
+            DateTime graduationDate = new DateTime(2023, 6, 30);
+            var IstudentT = new ImmutableStudent(123456, "Thomas", "Wolgast", startDate, endDate, graduationDate);
+            
+            DateTime startDateM = new DateTime(2020, 8, 30);
+            var IstudentM = new ImmutableStudent(12345, "My", "Jensen", startDateM, endDate, graduationDate);
+            
+            //Act
+            
+            //Assert
+            Assert.True(IstudentM.StartDate.Date == IstudentT.StartDate.Date);
+        }
+        
+        [Fact]
+        public void Accessing_ImmutabelStudents_GraduationDate_ToString_is_30_6_2023()
+        {
+            //arrange
+            DateTime startDate = new DateTime(2020, 8, 30);
+            DateTime endDate = new DateTime(2023, 6, 17);
+            DateTime graduationDate = new DateTime(2023, 6, 30);
+            var IstudentT = new ImmutableStudent(123456, "Thomas", "Wolgast", startDate, endDate, graduationDate);
+            //Act
+            
+            //Assert
+            Assert.Equal("30/6/2023",IstudentT.GraduationDate.ToString("dd/MM/yyyy"));
+        }
+        [Fact]
+        public void Accessing_ImmutabelStudents_id_is_123456()
+        {
+            //arrange
+            DateTime startDate = new DateTime(2020, 8, 30);
+            DateTime endDate = new DateTime(2023, 6, 17);
+            DateTime graduationDate = new DateTime(2023, 6, 30);
+            var IstudentT = new ImmutableStudent(123456, "Thomas", "Wolgast", startDate, endDate, graduationDate);
+            //Act
+                
+            //Assert
+            Assert.Equal(123456,IstudentT.Id);
         }
     }
 }
--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -15,6 +15,7 @@ namespace Assignment2
         public DateTime EndDate{get; set;}
         public DateTime GraduationDate{get; set;}
         
+        // unnused should be removed?
         public Status Status
         {
             get => _status;
@@ -51,11 +52,55 @@ namespace Assignment2
 
         }
     }
-
     public enum Status{
         New,
         Active,
         Dropout,
         Graduated
     }
+    
+    public record ImmutableStudent
+    {
+        private Status _status;
+        public ImmutableStudent(int id, string name, string surName, DateTime startDate, DateTime endDate, DateTime graduationDate) 
+            => (Id, Name, SurName, StartDate, EndDate, GraduationDate) = (id, name, surName, startDate, endDate, graduationDate);
+
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string SurName { get; init; }
+        public DateTime StartDate { get; init; }
+        public DateTime EndDate { get; init; }
+        public DateTime GraduationDate { get; init; }
+
+        public override string ToString()
+        {
+            _status = EvalStatus();
+            return $"id: {Id}, name: {Name} {SurName}, status: {_status}, {StartDate.ToString("dd/MM/yyyy")}, end date: {EndDate.ToString("dd/MM/yyyy")}, graduation date: {GraduationDate.ToString("dd/MM/yyyy")}";
+        }
+        
+        private Status EvalStatus() 
+        {
+            var now = DateTime.Now;
+            
+            if (now.Date == StartDate.Date)
+            {
+                return Status.New;
+            }
+            else if (GraduationDate.Date <= now.Date)
+            {
+                return Status.Graduated;
+            }
+            else if (EndDate.Date < now.Date && GraduationDate.Date > now.Date)
+            {
+                return Status.Dropout;
+            }
+            else
+            {
+                return Status.Active;
+            }
+
+        }
+    }
+
+    
 }
